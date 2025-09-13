7e2cb1d95233bca1af96ed8d5f867c41510a1df1
AthirK <athirkazem@gmail.com>
2025-09-13 22:49:11 +0200

    Add fetchArticlesFromAPI function and update button to trigger API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { Button } from "@/components/ui/button"
 
+const fetchArticlesFromAPI = async () => {
+  try {
+    const apiResponse = await fetch('https://dummyjson.com/posts');
+    const dummyData = await apiResponse.json();
+    console.log(dummyData.posts);
+  } catch (error) {
+    console.error("Kunde inte hämta artiklar:", error);
+  }
+};
+
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+      <Button onClick={fetchArticlesFromAPI}>Click me</Button>
     </div>
   )
 }
